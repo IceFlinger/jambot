8ae32790bc7aec2365b08acb6247957859d61a24
IceFlinger <ice@loud.house>
2016-02-22 10:33:29 -0500

    swampbot

---
+++ sample.py
@@ -0,0 +1,13 @@
+from swampbot import botModule
+#Small sample module
+class moduleClass(botModule):
+	def initialize(self):
+		pass
+	def on_join(self, c, e):
+		pass
+	def on_privmsg(self, c, e):
+		pass
+	def do_command(self, c, e, command, args, admin):
+		pass
+	def on_pubmsg(self, c, e):
+		pass
---
+++ sample_full.py
@@ -0,0 +1,39 @@
+from swampbot import botModule
+#Full sample module
+class moduleClass(botModule):
+	def initialize(self):
+		pass
+	def on_join(self, c, e):
+		pass
+	def on_nick(self, c, e):
+		pass
+	def on_mode(self, c, e):
+		pass
+	def on_welcome(self, c, e):
+		pass
+	def on_part(self, c, e):
+		pass
+	def on_ping(self, c, e):
+		pass
+	def on_privmsg(self, c, e):
+		pass
+	def on_privnotice(self, c, e):
+		pass
+	def do_command(self, c, e, command, args, admin):
+		pass
+	def on_pubmsg(self, c, e):
+		pass
+	def on_pubnotice(self, c, e):
+		pass
+	def on_quit(self, c, e):
+		pass
+	def on_invite(self, c, e):
+		pass
+	def on_pong(self, c, e):
+		pass
+	def on_action(self, c, e):
+		pass
+	def on_topic(self, c, e):
+		pass
+	def on_nick(self, c, e):
+		pass
