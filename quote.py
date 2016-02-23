8e62980625b61c8d4b39ad3d68f3506af11ab707
UVClay <clayallen3@gmail.com>
2016-02-23 10:45:09 -0500

    No more globals, cleaned up comments

--- quote.py
+++ quote.py
@@ -1,29 +1,14 @@
 from jambot import botModule
 #Quote module
-
 class moduleClass(botModule):
     def on_start(self):
-        global buffernick
-        global buffermsg
-        #global radbuffer
-        buffernick = 'wew'
-        buffermsg = 'wew'
-        #radbuffer = 'wew'
+        self.buffernick = 'wew'
+        self.buffermsg = 'wew'
     pass
 
     def on_pubmsg(self, c, e):
-        global buffernick
-        global buffermsg
-        #global radbuffer
-        #print('buffernick 1: ' + buffernick)
-        #print('buffermsg 1: ' + buffermsg)
-        #print(e)
-        buffernick = e.source.nick
-        buffermsg = e.arguments[0]
-        #print('buffernick 2: ' + buffernick)
-        #print('buffermsg 2: ' + buffermsg)
-        #if e.source.nick == 'Radbot':
-        #    radbuffer == e.arguments[0]
+        self.buffernick = e.source.nick
+        self.buffermsg = e.arguments[0]
         pass
 
     def on_send(self, chan, msg, modulename):
@@ -34,19 +19,10 @@ class moduleClass(botModule):
 
     def do_command(self, c, e, command, args, admin):
         if ((command == "q") or (command == "quote")):
-            global buffernick
-            global buffermsg
-            if buffernick and buffermsg == 'wew':
+            if self.buffernick and self.buffermsg == 'wew':
                 self.send(e.target, "No quote stored")
-            elif buffermsg == '!quote':
+            elif self.buffermsg == '!quote':
                 self.send(e.target, "No quote stored")
             else:
-                self.send(e.target, "Quote: " + buffernick + ": " + buffermsg)
+                self.send(e.target, "Quote: " + self.buffernick + ": " + self.buffermsg)
         pass
-        #if (command == "radquote"):
-        #    global radbuffer
-        #    if radbuffer == 'wew':
-        #        self.send(e.target, "No Radquote")
-        #    else:
-        #        self.send(e.target, "Radquote: " + radbuffer)
-        #    pass
