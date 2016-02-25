0de6df8e20f220e9c63353acb4a04e0a4fc0103f
UVClay <clayallen3@gmail.com>
2016-02-24 22:07:43 -0500

    Added say module

---
+++ modules/say.py
@@ -0,0 +1,28 @@
+from jambot import botModule
+
+
+# Quote module
+class moduleClass(botModule):
+    def on_start(self, c, e):
+        pass
+
+    def on_load_db(self):
+        pass
+
+    def on_pubmsg(self, c, e):
+        pass
+
+    def on_send(self, chan, msg, modulename):
+        self.buffernick = "jambot"
+        self.buffermsg = msg
+
+    def on_event(self, c, e):
+        pass
+
+    def do_command(self, c, e, command, args, admin):
+        if (command == "say"):
+            say = str.split(e.arguments[0], " ", maxsplit=1)
+            self.send(e.target, say[1])
+
+    def shutdown(self):
+        pass
