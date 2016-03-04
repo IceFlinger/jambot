b34d89306ce49ff1f60db63849b89fe5fb1695cd
UVClay <clayallen3@gmail.com>
2016-03-04 12:40:11 -0500

    Update notifier module v0.10

--- modules/update.py
+++ modules/update.py
@@ -1,6 +1,7 @@
 import git
 import os
 import time
+import threading
 from jambot import botModule
 
 
@@ -9,24 +10,10 @@ from jambot import botModule
 
 class moduleClass(botModule):
     def on_start(self, c, e):
-        self.dir = os.getcwd()
-        repo = git.Repo(self.dir)
-        assert not repo.bare
-        update_notified = 0
-        while True:
-            commits = repo.iter_commits('master..origin/master')
-            count = sum(1 for commit in commits)
-            if update_notified != 1:
-                print("git check")
-                if count >= 1:
-                    self.send(e.target, "Update available for jambot!")
-                    update_notified = 1
-                else:
-                    print("no update available")
-                    time.sleep(60)
-            else:
-                print("git update already notified, pass")
-                pass
+        self.channels = []
+        dir = os.getcwd()
+        self.repo = git.Repo(dir)
+        assert not self.repo.bare
         pass
 
     def on_load_db(self):
@@ -39,9 +26,40 @@ class moduleClass(botModule):
         pass
 
     def on_event(self, c, e):
+        if e.source.split("!")[0] == c.nickname:
+            if e.type == "join":
+                self.channels.append(e.target)
+            elif e.type == "part":
+                self.channels.remove(e.target)
+
+        self.update_notified = 0
+        while True:
+            self.repo.git.fetch()
+            commits = self.repo.iter_commits('master..origin/master')
+            count = sum(1 for c in commits)
+            if self.update_notified != 1:
+                print("git check")
+                if count >= 1:
+                    for channel in self.channels:
+                        self.send(channel, "Update available for jambot!")
+                    self.update_notified = 1
+                else:
+                    print("no update available")
+                    threading.Thread.run(time.sleep(60))
+            else:
+                print("git update already notified, stop")
+                return
         pass
 
     def do_command(self, c, e, command, args, admin):
+        if (command == "updatecheck"):
+            print("why")
+            if self.update_notified == 1:
+                self.send(e.target, "Update available for jambot!")
+            elif self.update_notified == 0:
+                self.send(e.target, "Jambot is up to date!")
+            else:
+                self.send(e.target, "You somehow broke the update module.")
         pass
 
     def shutdown(self):
